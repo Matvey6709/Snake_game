392e13388f587fb3a589955372772f0f93e4e2b7
matve <MatveyKolpakov@yandex.ru>
2022-04-18 17:57:24 +0500

    code refactoring23

--- core/src/datamanager/ServerUpdate.java
+++ core/src/datamanager/ServerUpdate.java
@@ -25,7 +25,7 @@ public class ServerUpdate {
         cells.add(new Cell(0, 0, size.getWidthGame(100), size.getHeightGame(100)));
 
         texture = new Texture("BlueS2.png");
-        //dd
+        //dded
     }
 
     public void render(Player players, Snake share) {
